64e80bf1c25f6ee00e5e5e671725992a9f8d5515
kingkong24 <wnpp234@naver.com>
2023-05-04 13:33:24 +0900

    13:35
    ing

--- Assets/3.Script/ETC/RankingSystem.cs
+++ Assets/3.Script/ETC/RankingSystem.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RankingSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 랭킹을 저장할 자료구조
+    public struct Ranking
     {
-        
+        public string name;
+        public string score;
+
+        public Ranking(string name, string score)
+        {
+            this.name = name;
+            this.score = score;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("InputName_UI")]
+    [SerializeField] private GameObject inputName_UI;
+    [SerializeField] private InputField inputName_InputField;
+
+    [Header("Ranking_UI")]
+    [SerializeField] private GameObject ranking_UI;
+    [SerializeField] private Text ranking_text;
+
+
 }
